6fdeb6633129086f6a91cc200c1cf95bfdbb526d
Irakli Adamashvili <adamashvili.irakli8@gmail.com>
2023-05-02 19:53:12 +0400

    add navbar and left side of header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Hero.css'
 import Header from '../Header/Header'
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 
 const Hero = () => {
   return (
@@ -10,6 +14,40 @@ const Hero = () => {
             <div className="the-best-ad">
                 <div></div>
                 <span>The best fitness club in the town</span>
+            </div>
+            {/* Head */}
+            <div className="hero-text">
+                <div>
+                    <span className="stroke-text"> Shape </span>
+                    <span>Your</span>
+                    </div>
+
+                    <div><span>Ideal body</span></div>
+                    <div>
+                        <span>in here we will help you to shape and build your ideal body and live up your life to fullest</span>
+                    </div>
+            </div>
+
+            {/* Figures */}
+            <div className="figures">
+                <div>
+                     <span>+140</span>
+                      <span>expert coachs</span> 
+                      </div>
+                <div>
+                     <span>+978</span>
+                      <span>members joined</span> 
+                      </div>
+                <div>
+                     <span>+50</span>
+                     <span>fitness programs</span> 
+                </div>    
+            </div>
+
+            {/* buttons */}
+            <div className="hero-buttons">
+                <button className="btn">Get Started</button>
+                <button className="btn">Learn More</button>
             </div>
         </div>
         <div className="right-h"></div>
