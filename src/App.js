56efa24137e18fe13508ce63ee535145fca68696
Irakli Adamashvili <adamashvili.irakli8@gmail.com>
2023-05-05 17:55:39 +0400

    add plans cards from data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Rasons from './components/Reasons/Rasons';
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Hero/>
       <Programs/>
       <Rasons/>
+      <Plans/>
     </div>
   );
 }
